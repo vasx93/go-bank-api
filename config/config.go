49ff396fd43d0e8af9347672f789c3340788957c
Vasko Raketic <vasx93@hotmail.com>
2024-07-31 18:04:29 +0200

    load envs

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading environment variables")
+	}
+
+}
